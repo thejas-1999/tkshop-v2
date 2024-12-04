44f57672eb228537fcd6e0a71dd55a32d0b5c6d4
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2024-12-04 13:42:43 +0530

    create the footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
+import { Container } from "react-bootstrap";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
-      <h1>Hello world</h1>
+      <main>
+        <Container>
+          <h1>Hello world</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
