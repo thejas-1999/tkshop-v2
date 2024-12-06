9e51f5b9f991be4a88cbdf5fe90ed824260304e5
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2024-12-06 10:52:33 +0530

    create the productScreen for displaying individual products

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,11 +13,13 @@ import "./assets/styles/bootstrap.custom.css";
 /* import "bootstrap/dist/css/bootstrap.min.css"; */
 import App from "./App.jsx";
 import HomeScreen from "./pages/HomeScreen.jsx";
+import ProductScreen from "./pages/ProductScreen.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
     </Route>
   )
 );
