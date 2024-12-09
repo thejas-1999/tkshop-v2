c91947dba4dbd0a2d83389faf0e561289d8cb6cf
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2024-12-09 11:15:26 +0530

    create a dummy user data

---
+++ backend/data/user.js
@@ -0,0 +1,24 @@
+import bcrypt from "bcryptjs";
+
+const users = [
+  {
+    name: "Admin user",
+    email: "admin@example.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "Thejas",
+    email: "thejas@example.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: false,
+  },
+  {
+    name: "Arun",
+    email: "arun@example.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: false,
+  },
+];
+
+export default users;
