1037276576d6399b43eaf9954c172a62d0a7a0ef
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2024-12-10 10:30:47 +0530

    create a custom error handler function

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 
 import connectDB from "./config/connectDB.js";
 import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -20,4 +21,7 @@ const startServer = () => {
     console.log(error);
   }
 };
+
+app.use(notFound);
+app.use(errorHandler);
 startServer();
