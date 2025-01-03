5c52fb36acf29a939fd35b7e41d9af261ca2cd8d
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2025-01-03 14:38:17 +0530

    initialize the bootstrap create the header and footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">TKShop &copy; {currentYear}</Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+export default Footer;
