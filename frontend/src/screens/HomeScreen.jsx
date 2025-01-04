28b0ae53eeda1d86f76fe9dade69b6e8cd410247
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2025-01-04 13:43:02 +0530

    create a product card for each products

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,13 +1,14 @@
 import { Row, Col } from "react-bootstrap";
 import products from "../products";
+import Product from "../components/Product";
 
 const HomeScreen = () => {
   return (
     <>
       <Row>
         {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3}>
-            {product.name}
+          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+            <Product product={product} />
           </Col>
         ))}
       </Row>
