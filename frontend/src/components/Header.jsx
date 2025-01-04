7437e21a399778a4b79803f0ca74be6d0220d0f6
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2025-01-04 14:45:28 +0530

    Add react router into the project

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,7 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaUser, FaShoppingCart } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 
 const Header = () => {
@@ -7,22 +9,29 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">
-            <img
-              src={logo}
-              style={{ height: "60px", borderRadius: "20px" }}
-              alt=""
-            />{" "}
-            TK shop
-          </Navbar.Brand>
+          <Link className="custom-link">
+            <Navbar.Brand>
+              <img
+                src={logo}
+                style={{ height: "60px", borderRadius: "20px" }}
+                alt=""
+              />{" "}
+              TK shop
+            </Navbar.Brand>
+          </Link>
+
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart">
-                <FaShoppingCart /> Cart
+              <Nav.Link>
+                <Link to="/cart" className="custom-link">
+                  <FaShoppingCart /> Cart
+                </Link>
               </Nav.Link>
-              <Nav.Link href="/login">
-                <FaUser /> Sign In
+              <Nav.Link>
+                <Link to="/login" className="custom-link">
+                  <FaUser /> Sign In
+                </Link>
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,28 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
 import "./assets/styles/index.css";
 import "./assets/styles/bootstrap.custom.css";
 
 import App from "./App.jsx";
+import HomeScreen from "./screens/HomeScreen.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+    </Route>
+  )
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
