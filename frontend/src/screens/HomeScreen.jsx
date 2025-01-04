08a9dbc2c57146c116cc8cef3dad3b80b3b82e00
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2025-01-04 12:59:31 +0530

    create a homeScreen for the project

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,4 +1,17 @@
+import { Row, Col } from "react-bootstrap";
+import products from "../products";
+
 const HomeScreen = () => {
-  return <div>HomeScreen</div>;
+  return (
+    <>
+      <Row>
+        {products.map((product) => (
+          <Col sm={12} md={6} lg={4} xl={3}>
+            {product.name}
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
 export default HomeScreen;
