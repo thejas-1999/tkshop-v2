e5a2213be1d43880b43a12ac78e70f12971f9377
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2025-01-08 16:33:50 +0530

    create the jwt token and put in to a cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/userModel.js";
 import asyncHandler from "../middleware/asyncHandler.js";
+import jwt from "jsonwebtoken";
 
 //@desc Authendicate the user and get token
 //@route post api/users/login
@@ -9,6 +10,17 @@ const loginUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+
+    //setjwt as http-only cookie
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
+    });
     res.json({
       _id: user._id,
       name: user.name,
