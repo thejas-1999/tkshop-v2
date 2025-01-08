3d8279ac37cb48bc67b7ae7bdb9d0c7b66960eb6
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2025-01-08 15:23:46 +0530

    create the userrouter and controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,77 @@
+import User from "../models/userModel.js";
+import asyncHandler from "../middleware/asyncHandler.js";
+
+//@desc Authendicate the user and get token
+//@route post api/users/login
+//@access public
+const loginUser = asyncHandler(async (req, res) => {
+  res.send("Login");
+});
+
+//@desc register the user
+//@route post api/users
+//@access public
+const registerUser = asyncHandler(async (req, res) => {
+  res.send("Register");
+});
+
+//@desc logout the user
+//@route post api/users/logout
+//@access private
+const logoutUser = asyncHandler(async (req, res) => {
+  res.send("Logout");
+});
+
+//@desc get the user Profile
+//@route get api/users/profile
+//@access private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.send("profile");
+});
+
+//@desc update the user
+//@route put api/users/profile
+//@access private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.send("update profile");
+});
+
+//@desc get all users
+//@route get api/users/
+//@access private/admin
+const getUsers = asyncHandler(async (req, res) => {
+  res.send("get all users by admin");
+});
+
+//@desc delete user
+//@route delete api/users/:id
+//@access private/admin
+const deleteUser = asyncHandler(async (req, res) => {
+  res.send("delete user by id");
+});
+
+//@desc get  user by id
+//@route get api/users/:id
+//@access private/admin
+const getUserById = asyncHandler(async (req, res) => {
+  res.send("get  user profile by admin");
+});
+
+//@desc update  user by id
+//@route put api/users/:id
+//@access private/admin
+const updateUser = asyncHandler(async (req, res) => {
+  res.send("update user by admin");
+});
+
+export {
+  loginUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+};
--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -26,6 +26,10 @@ const CartScreen = () => {
     dispatch(removeFromCart(id));
   };
 
+  const checkoutHandler = () => {
+    navigate("/login?redirect=/shipping");
+  };
+
   return (
     <Row>
       <Col md={8}>
@@ -93,6 +97,7 @@ const CartScreen = () => {
                 type="button"
                 className="btn-block"
                 disabled={cartItems.length === 0}
+                onClick={checkoutHandler}
               >
                 Proceed to Checkout
               </Button>
