24afafe8c0113046f1e672bd4df85fb6694f4ca5
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2025-01-09 10:42:20 +0530

    logout and clear cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -44,7 +44,11 @@ const registerUser = asyncHandler(async (req, res) => {
 //@route post api/users/logout
 //@access private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send("Logout");
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: `Logged out successfully` });
 });
 
 //@desc get the user Profile
