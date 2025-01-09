c607662e082d33e0c5a95e1658692dc0fdc61da2
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2025-01-09 11:39:15 +0530

    user profile end points

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,7 +11,7 @@ const loginUser = asyncHandler(async (req, res) => {
 
   if (user && (await user.matchPassword(password))) {
     generateToken(res, user._id);
-    res.json({
+    res.status(200).json({
       _id: user._id,
       name: user.name,
       email: user.email,
@@ -44,7 +44,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
   if (user) {
     generateToken(res, user._id);
-    res.status(201).json({
+    res.status(200).json({
       id: user._id,
       name: user.name,
       email: user.email,
@@ -71,14 +71,46 @@ const logoutUser = asyncHandler(async (req, res) => {
 //@route get api/users/profile
 //@access private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send("profile");
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.status(200).json({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 //@desc update the user
 //@route put api/users/profile
 //@access private
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.send("update profile");
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 //@desc get all users
