5339064dd5245743044b873b4d5fd0eea8b5a086
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2025-01-09 10:28:49 +0530

    create the auth middleware and add to the end points

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -14,6 +16,9 @@ const app = express();
 //middlwares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//cookie parser
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("API is running");
