f4d965a7305a996a3d49bdf4ae28b19a7234778e
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2025-01-15 11:26:32 +0530

    create the order page

---
+++ frontend/src/screens/OrderScreen.jsx
@@ -0,0 +1,139 @@
+import { Link, useParams } from "react-router-dom";
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from "react-bootstrap";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { useGetOrderDetailsQuery } from "../slices/orderApiSlice";
+
+const OrderScreen = () => {
+  const { id: orderId } = useParams();
+
+  const {
+    data: order,
+    refetch,
+    isLoading,
+    error,
+  } = useGetOrderDetailsQuery(orderId);
+  console.log(order);
+
+  const PaymentHandler = () => {};
+
+  return isLoading ? (
+    <Loader />
+  ) : error ? (
+    <Message variant="danger" />
+  ) : (
+    <>
+      <h1>Order {order._id}</h1>
+      <Row>
+        <Col md={8}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>Order Items</h2>
+              {order.orderItems.map((item) => (
+                <ListGroup.Item key={item._id}>
+                  <Row>
+                    <Col md={1}>
+                      <Image src={item.image} alt={item.name} fluid rounded />
+                    </Col>
+                    <Col>
+                      <Link to={`/product/${item._id}`}>{item.name}</Link>
+                    </Col>
+                    <Col md={4}>
+                      {item.price} x {item.qty} = {item.price * item.qty}₹
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              ))}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h2>Shipping Adress</h2>
+              <p>
+                <strong>Name:</strong> {order.user.name}
+              </p>
+              <p>
+                <strong>Email:</strong> {order.user.email}
+              </p>
+              <p>
+                <strong>Address:</strong> {order.shippingAddress.address},
+                {order.shippingAddress.city}-{order.shippingAddress.postalCode},
+                {order.shippingAddress.country}
+              </p>
+              {order.isDelivered ? (
+                <Message variant="success">
+                  Delivered on {order.deliveredAt}
+                </Message>
+              ) : (
+                <Message variant="danger">Not Delivered</Message>
+              )}
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h2>Payment</h2>
+              <p>
+                <strong>Payment Method:</strong> {order.paymentMethod}
+              </p>
+              {order.isPaid ? (
+                <Message variant="success">Paid on {order.paidAt}</Message>
+              ) : (
+                <Message variant="danger">Not Paid</Message>
+              )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h2>Order Summary</h2>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Items Price:</Col>
+                  <Col>{order.itemsPrice}₹</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Tax Price:</Col>
+                  <Col>{order.taxPrice}₹</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Shipping Charge:</Col>
+                  <Col>{order.shippingPrice}₹</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Total Price:</Col>
+                  <Col>{order.totalPrice}₹</Col>
+                </Row>
+              </ListGroup.Item>
+
+              {/*  <ListGroup.Item>
+                <Button
+                  type="button"
+                  className="btn-block"
+                  disabled={order.isPaid}
+                  onClick={PaymentHandler}
+                >
+                  Pay
+                </Button>
+              </ListGroup.Item> */}
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+export default OrderScreen;
