a2b6fb1ca9cfe47566c488228568077ca204b867
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2025-01-22 14:55:21 +0530

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -39,6 +39,22 @@ app.get("/api/config/paypal", (req, res) =>
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
+// Check if the environment is production
+if (process.env.NODE_ENV === "production") {
+  // Serve static files from the Vite build directory
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+  // Handle React routing: serve the index.html for all other routes
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+} else {
+  // Development mode
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
