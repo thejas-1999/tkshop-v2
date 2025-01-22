f57732b4fc1d8a271387eeee9230c0a0d03aa5bf
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2025-01-22 09:51:08 +0530

    create product review-backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -68,7 +68,7 @@ const updateProduct = asyncHandler(async (req, res) => {
 
 //@desc delete product
 //@route delete /api/products/:id
-//@access public
+//@access private/admin
 const deleteProduct = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
 
@@ -81,10 +81,50 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
+//@desc create a new review
+//@route post /api/products/:id/reviews
+//@access private
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (review) => review.user.toString() === req.user._id.toString()
+    );
+
+    if (alreadyReviewed) {
+      res.status(400);
+      throw new Error("Product already Reviewed");
+    }
+
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    };
+
+    product.reviews.push(review);
+
+    product.numReviews = product.reviews.length;
+
+    product.rating =
+      product.reviews.reduce((acc, review) => acc + review.rating, 0) /
+      product.reviews.length;
+    await product.save();
+    res.status(200).json({ message: "Review Added" });
+  } else {
+    res.status(404);
+    throw new Error("Resource is not found");
+  }
+});
+
 export {
   getProducts,
   getProductById,
   createProduct,
   updateProduct,
   deleteProduct,
+  createProductReview,
 };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   createProduct,
+  createProductReview,
   deleteProduct,
   getProductById,
   getProducts,
@@ -16,5 +17,7 @@ router
   .get(getProductById)
   .put(protect, admin, updateProduct)
   .delete(protect, admin, deleteProduct);
+
+router.post("/:id/reviews", protect, createProductReview);
 
 export default router;
