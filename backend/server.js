e7ec65b1ac92645e003b8def7081e9e5ac95b7b5
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2025-04-29 10:16:52 +0530

    change some issues

--- backend/server.js
+++ backend/server.js
@@ -35,10 +35,16 @@ app.get("/api/config/paypal", (req, res) =>
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-app.use(express.static(path.join(__dirname, "frontend/dist")));
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "frontend/dist", "index.html"))
-);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
